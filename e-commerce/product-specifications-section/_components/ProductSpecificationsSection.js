9e27c6ded5b6c26fd6e050fb60a53e0c839601a9
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-09-03 08:50:33 +0300

    change primary heading's font

--- e-commerce/product-specifications-section/_components/ProductSpecificationsSection.js
+++ e-commerce/product-specifications-section/_components/ProductSpecificationsSection.js
@@ -10,7 +10,7 @@ function ProductsSpecificationsSection({ specifications }) {
   return (
     <div className="max-w-[1280px] grid grid-rows-[auto_auto] min-[1440px]:grid-rows-[156px_auto] gap-16 mx-auto">
       <header>
-        <h2 className="text-3xl md:text-5xl font-medium text-neutral-900 mb-6">Discover timeless elegance</h2>
+        <h2 className="text-3xl md:text-5xl font-semibold text-neutral-900 mb-6">Discover timeless elegance</h2>
         <p className="text-lg text-neutral-600">
           Step into a world where quality meets quintessential charm with our collection. Every thread weaves a promise of unparalleled
           quality, ensuring that each garment is not just a part of your wardrobe, but a piece of art. Here&apos;s the essence of what makes
