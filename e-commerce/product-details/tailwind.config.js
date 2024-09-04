1dbded6cf03dd1a24554364413346943a0c55b4b
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-09-04 07:33:49 +0300

    initial commit

---
+++ e-commerce/product-details/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./_components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      backgroundImage: {},
+    },
+  },
+  plugins: [],
+};
