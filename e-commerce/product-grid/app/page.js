ba08435234ddb1f3c914c0c4e711ca94e5eeeaa1
Dogukan Aysan <dogukanaysan01@gmail.com>
2024-09-15 03:40:09 +0300

    start implementing product grid section

---
+++ e-commerce/product-grid/app/page.js
@@ -0,0 +1,10 @@
+import MainContainer from "./_components/layout/MainContainer";
+import SectionContainer from "./_components/layout/SectionContainer";
+
+export default function Home() {
+  return (
+    <MainContainer>
+      <SectionContainer></SectionContainer>
+    </MainContainer>
+  );
+}
